6d257361f58c409728c2cc14e32d5754e9bebbec
Marc Scheu <marc.scheu@web.de>
2025-10-27 21:13:09 +0000

    edit button (4)

--- articler/webapp/ext/controller/ListReportExt.controller.js
+++ articler/webapp/ext/controller/ListReportExt.controller.js
@@ -1,9 +1,17 @@
+const { cloneElement } = require("react");
+
 sap.ui.define([
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/ColumnListItem"
 ], function(MessageToast) {
     'use strict';
 
     return {
+        onInit: function(oEvent) {            
+            this.oTableDisplay = this.getView().byId("Article::sap.suite.ui.generic.template.ListReport");
+            this.oTableEdit = deepClone(this.oTableDisplay);
+        },
+
         onEdit: function(oEvent) {
             MessageToast.show("Custom handler invoked.");
         }
