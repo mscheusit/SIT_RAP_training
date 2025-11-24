8568159433ff60049011d33eab39cb4ef802a2ab
Marc Scheu <marc.scheu@web.de>
2025-11-24 10:27:23 +0000

    Test

---
+++ articler/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zsitc.articler.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
